7160a694677a27028c7fa94252290a3067db2ebf
Katherine <Katherine@gmail.com>
2025-07-08 11:20:03 -0600

    Swagger pero falta conectar2

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,20 @@ import "./database.js";
 import clientRoutes from "./src/routes/client.js";
 import reservationRoutes from "./src/routes/reservation.js";
 import registerClientRouter from "./src/routes/registerClient.js";
+import swaggerUi from "swagger-ui-express"; 
+import fs from "fs"; 
+import path from "path"; 
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./partplus.json"), "utf-8")
+)
 
+app.use("/api/docs", swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 app.use("/api/client", clientRoutes);
 app.use("/api/reservation", reservationRoutes);
 app.use("/api/registerClient", registerClientRouter);
